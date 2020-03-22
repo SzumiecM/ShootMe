b4ad7d5fce24f50af8026b07226deabcffc168dc
mariakowalczyk2016 <44197788+mariakowalczyk2016@users.noreply.github.com>
2020-03-22 21:29:00 +0100

    first commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+import pygame
+
+screen_TITLE = "Shooter 2D"
+screen_SIZE = 1000, 500
+screen_CENTER = screen_SIZE[0] // 2, screen_SIZE[1] // 2
+
+
+# colors ----------------------------------------------------------------------------
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+
+LIGHT_ORANGE = 255, 180, 90
+ORANGE = 255, 127, 80
+DARK_ORANGE = 160, 85, 70
+
+RED = 255, 10, 10
